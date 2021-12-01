c4e8e594f1c063994f1fc806442cbf62b50a8eca
sooof <sooof@me.com>
2021-12-01 01:26:53 -0800

    all class  work

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,22 +1,40 @@
 import React from 'react';
 
-const Todo = props => {
+// const Todo = props => {
 
-//   return (
-//     <div className={`item${props.item.completed ? ' completed' : ''}`}>
-//       <p>{props.item.task}</p>
+// //   return (
+// //     <div className={`item${props.item.completed ? ' completed' : ''}`}>
+// //       <p>{props.item.task}</p>
+// //     </div>
+// //   );
+
+//   const handleClick = () =>{
+//     console.log("Todo")
+//     // props.handleToggleItem(props.item)
+// }
+//     return (
+//     <div onClick={handleClick} className={`item${props.item.completed ? ' completed' : ''}`}>
+//     <p>{props.item.task}</p>
 //     </div>
-//   );
+// );
+// };
 
-  const handleClick = () =>{
-    console.log("Todo")
-    props.handleToggleItem(props.item)
-}
-    return (
-    <div onClick={handleClick} className={`item${props.item.completed ? ' completed' : ''}`}>
-    <p>{props.item.task}</p>
-    </div>
-);
+// export default Todo;
+
+
+
+class Todo extends React.Component {
+
+    handleClick = () =>{
+        this.props.handleToggleItem(this.props.item)
+    }
+    render(){
+        return (
+            <div onClick={this.handleClick} className={`item${this.props.item.completed ? ' completed' : ''}`}>
+              <p>{this.props.item.task}</p>
+            </div>
+          );
+    }
 };
 
-export default Todo;
+export default Todo ;
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -6,31 +6,32 @@
 
     import Todo from "./Todo";
     
-    const TodoList = props => {
+    // const TodoList = props => {
      
-      return (
-        <div className="todo-list">
-          {props.todolists.map(item => (
-            <Todo  handleToggleItem={props.handleToggleItem} key={item.id} item={item} />
-          ))}
-        </div>
-      );
-    };
+    //   return (
+    //     <div className="todo-list">
+    //       {props.todolists.map(item => (
+    //         <Todo  handleToggleItem={props.handleToggleItem} key={item.id} item={item} />
+    //       ))}
+    //     </div>
+    //   );
+    // };
     
+    // export default TodoList;
+    class TodoList extends React.Component {
+        render(){
+            // console.log("TodoList = ", this.props.todolists)
+            return(
+                <div className="todo-list">
+                    {/* <p>TodoList</p> */}
+                   {
+                       this.props.todolists.map(item=> 
+                        // <Item key={item.id} item={item} />
+                        <Todo handleToggleItem={this.props.handleToggleItem} key={item.id} item={item} />
+                        )
+                   } 
+                </div>
+            )
+        }
+    }
     export default TodoList;
-  
-  //     render(){
-  //         console.log("TodoList = ", this.props.todolists)
-  //         return(
-  //             <div>
-  //                 <p>TodoList</p>
-  //                {
-  //                    this.props.todolists.map(item=> 
-  //                     <Item key={item.id} item={item} />
-  //                     )
-  //                } 
-  //             </div>
-  //         )
-  //     }
-  // }
-  // export default TodoList;
